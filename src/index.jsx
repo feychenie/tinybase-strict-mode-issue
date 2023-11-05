3269308d12ec2a607dc51fa9c1fd9057fd28c14b
Frederik Eychenié <277455+feychenie@users.noreply.github.com>
2023-11-05 15:46:40 +0100

    issue reproduction

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
-import './index.css';
-import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { StrictMode } from "react";
 
-addEventListener('load', () =>
-  ReactDOM.createRoot(document.getElementById('app')).render(<App />)
+addEventListener("load", () =>
+  ReactDOM.createRoot(document.getElementById("app")).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
 );
